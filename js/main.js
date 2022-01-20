c6ab7642cac5af6fe0e0e76ca35c45fd504fc65a
dimmat96 <dimmat96@gmail.com>
2022-01-20 21:18:45 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -4,11 +4,36 @@ console.log('global');
 "use strict";
 
 console.log('maxgraph');
+"use strict";
+
 /**
   * название функции
   *
   * @param {number} first - первое число
   * @returns {number}
   */
-"use strict";
+var tabsBtn = document.querySelectorAll(".tabs__nav-btn");
+var tabsItems = document.querySelectorAll(".tabs__item");
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+  item.addEventListener("click", function () {
+    var currentBtn = item;
+    var tabId = currentBtn.getAttribute("data-tab");
+    var currentTab = document.querySelector(tabId);
+
+    if (!currentBtn.classList.contains('active')) {
+      tabsBtn.forEach(function (item) {
+        item.classList.remove('active');
+      });
+      tabsItems.forEach(function (item) {
+        item.classList.remove('active');
+      });
+      currentBtn.classList.add('active');
+      currentTab.classList.add('active');
+    }
+  });
+}
+
+document.querySelector('.tabs__nav-btn').click();
 //# sourceMappingURL=main.js.map
